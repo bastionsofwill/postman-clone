6f0476dc2b82eb7aa7f533184ce40925dc4aa47d
bastionsofwill <bastionsofwill@gmail.com>
2022-09-01 18:29:36 +0900

    wip

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from "axios"
 
+const form = document.querySelector('[data-form]')
 const queryParamsContainer = document.querySelector('[data-query-params]')
 const requestHeadersContainer = document.querySelector('[data-request-headers]')
 const keyValueTemplate = document.querySelector('[data-key-value-template]')
+
+document
+    .querySelector('[data-add-query-param-btn]')
+    .addEventListener("click", () => {
+        queryParamsContainer.append(createKeyValuePair())
+    })
+document
+    .querySelector('[data-add-request-header-btn]')
+    .addEventListener("click", () => {
+        requestHeadersContainer.append(createKeyValuePair())
+    })
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    axios({
+        url: document.querySelector('[data-url]').value,
+        method: document.querySelector('[data-method]').value,
+        params: keyValuePairsToObjects(queryParamsContainer),
+        headers: keyValuePairsToObjects(requestHeadersContainer)
+    })
+})
 
 function createKeyValuePair() {
     const element = keyValueTemplate.content.cloneNode(true)
@@ -13,4 +37,12 @@ function createKeyValuePair() {
         e.target.closest('[data-key-value-pair]').remove()
     })
     return element
+}
+
+function keyValuePairsToObjects(container) {
+    const pairs = container.querySelectorAll('[data-key-value-pair]')
+    return [...pairs].reduce((data, pair) => {
+        const key = pair.querySelector('[data-key]').value
+        const value = pair.querySelector('[data-value]').value
+    })
 }
