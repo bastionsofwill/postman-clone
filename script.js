21dc25fc266217ebf14f0ebec52891da7d0679c3
bastionsofwill <bastionsofwill@gmail.com>
2022-09-02 16:45:18 +0900

    finish

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from "axios"
+import prettyBytes from 'pretty-bytes'
+import setupEditors from './setupEditor'
 
 const form = document.querySelector('[data-form]')
 const queryParamsContainer = document.querySelector('[data-query-params]')
 const requestHeadersContainer = document.querySelector('[data-request-headers]')
 const keyValueTemplate = document.querySelector('[data-key-value-template]')
-
+const responseHeadersContainer = document.querySelector("[data-response-headers]")
 document
     .querySelector('[data-add-query-param-btn]')
     .addEventListener("click", () => {
@@ -20,18 +22,69 @@ document
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
 
+axios.interceptors.request.use(request => {
+    request.customData = request.customData || {}
+    request.customData.startTime = new Date().getTime()
+    return request
+})
+
+function updateEndTime(response) {
+    response.customData = response.customData || {}
+    response.customData.time = new Date().getTime() - response.config.customData.startTime
+    return response
+}
+
+axios.interceptors.response.use(updateEndTime, e => {
+    return Promise.reject(updateEndTime(e.response))
+})
+
+const { requestEditor, updateResponseEditor } = setupEditors()
+
 form.addEventListener('submit', e => {
     e.preventDefault()
 
+    let data
+    try {
+        data = JSON.parse(requestEditor.state.doc.toString() || null)
+    } catch (e) {
+        console.error(e)
+        alert('JSON data is malformed')
+        return 
+    }
     axios({
         url: document.querySelector('[data-url]').value,
         method: document.querySelector('[data-method]').value,
         params: keyValuePairsToObjects(queryParamsContainer),
-        headers: keyValuePairsToObjects(requestHeadersContainer)
-    }).then(response => {
+        headers: keyValuePairsToObjects(requestHeadersContainer),
+        data
+    })
+    .catch(e => e)
+    .then(response => {
+        document.querySelector("[data-response-section]").classList.remove("d-none")
+        updateResponseDetails(response)
+        updateResponseEditor(response.data)
+        updateResponseHeaders(response.headers)
         console.log(response)
     })
 })
+function updateResponseHeaders(headers) {
+    responseHeadersContainer.innerHTML = ""
+    Object.entries(headers).forEach(([key, value]) => {
+        const keyElement = document.createElement('div')
+        keyElement.textContent = key
+        responseHeadersContainer.append(keyElement)
+        const valueElement = document.createElement('div')
+        valueElement.textContent = key
+        responseHeadersContainer.append(valueElement)
+    })
+}
+function updateResponseDetails(response) {
+   document.querySelector('[data-status]').textContent = response.status
+   document.querySelector('[data-time]').textContent = response.customData.time
+   document.querySelector('[data-size]').textContent = prettyBytes(
+        JSON.stringify(response.data).length + JSON.stringify(response.headers).length
+   )
+}
 
 function createKeyValuePair() {
     const element = keyValueTemplate.content.cloneNode(true)
---
+++ setupEditor.js
@@ -0,0 +1,42 @@
+import { basicSetup } from "codemirror";
+import { EditorState } from "@codemirror/state";
+import { defaultKeymap } from '@codemirror/commands';
+import { json } from "@codemirror/lang-json";
+import { EditorView, keymap } from "@codemirror/view";
+
+export default function setupEditors() {
+    const jsonRequestBody = document.querySelector('[data-json-request-body]')
+    const jsonResponseBody = document.querySelector('[data-json-response-body]')
+
+    const basicExtensions = [
+        basicSetup,
+        keymap.of([defaultKeymap]),
+        json(),
+        EditorState.tabSize.of(2)
+    ]
+    const requestEditor = new EditorView({
+        state: EditorState.create({
+            doc: "{\n\t\n}",
+            extensions: basicExtensions
+        }),
+        parent: jsonRequestBody
+    })
+    const responseEditor = new EditorView({
+        state: EditorState.create({
+            doc: "{}",
+            extensions: [...basicExtensions, EditorView.editable.of(false)]
+        }),
+        parent: jsonResponseBody
+    })
+
+    function updateResponseEditor(value) {
+       responseEditor.dispatch({
+        changes: {
+            from: 0,
+            to: responseEditor.state.doc.length,
+            insert: JSON.stringify(value, null, 2)
+        }
+       }) 
+    }
+    return { requestEditor, updateResponseEditor }
+}
